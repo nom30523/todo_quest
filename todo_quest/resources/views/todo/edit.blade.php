8136f8c0ee86ebd05f3781da7dc49b6ecae9a163
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-06-01 14:19:40 +0900

    form等のstyle調整

--- todo_quest/resources/views/todo/edit.blade.php
+++ todo_quest/resources/views/todo/edit.blade.php
@@ -2,11 +2,17 @@
 
 @section('content')
 <div class="container">
-  <h3>やることを編集する</h3>
   <form action="{{ action('TodosController@update', $todo) }}" method="post">
     @csrf
-    <input type="text" name="body" value="{{ $todo->body }}">
-    <input type="submit" value="変更">
+    <div class="row">
+      <label for="todo-content" style="padding-left: 15px;">やることを編集する</label>
+      <div class="form-group col-sm-10">
+        <input type="text" name="body" id="todo-content" class="form-control" value="{{ $todo->body }}">
+      </div>
+      <div class="col-sm-2">
+        <input type="submit" value="変更" class="btn btn-primary">
+      </div>
+    </div>
   </form>
 </div>
 @endsection
--- todo_quest/resources/views/todo/show.blade.php
+++ todo_quest/resources/views/todo/show.blade.php
@@ -5,47 +5,58 @@
   <div class="user_card">
     <p>{{ $user->id }}{{ $user->name }}</p>
   </div>
-  <h3>やることを追加する</h3>
   @if (count($errors) > 0)
-  <div>
-      <ul>
-          @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-          @endforeach
-      </ul>
-  </div>
+    <div class="alert alert-danger">
+      @foreach ($errors->all() as $error)
+        {{ $error }}
+      @endforeach
+    </div>
   @endif
   <form action="{{ action('TodosController@store', $user) }}" method="post">
     @csrf
-    <input type="text" name="body" value="{{ old('body') }}">
-    <input type="submit" value="追加">
+    <div class="row">
+      <label for="todo-content" style="padding-left: 15px;">やることを追加する</label>
+      <div class="form-group col-sm-10">
+        <input type="text" name="body" value="{{ old('body') }}" class="form-control" id="todo-content">
+      </div>
+      <div class="col-sm-2">
+        <input type="submit" value="追加" class="btn btn-primary">
+      </div>
+    </div>
   </form>
-  <div class="todo-lists">
-    <ul>
+  <div class="card">
+    <ul class="list-group list-group-flush">
       @foreach ($user->todos as $todo)
-        <li class="todo-list" style="list-style: none;">
-          <form action="{{ action('TodosController@status', $todo) }}" method="post" id="form_{{ $todo->id }}">
-            @csrf
-          </form>
-          @if ($todo->status == 0)
-            <a href="#" class="status" data-id="{{ $todo->id }}">
-              <i class="far fa-square"></i>
-            </a>
-          @else
-            <a href="#" class="status" data-id="{{ $todo->id }}">
-              <i class="far fa-check-square"></i>
-            </a>
-          @endif
-          {{ $todo->body }}
-          <a href="{{ action('TodosController@edit', $todo) }}">編集</a>
-          <form action="{{ action('TodosController@destroy', $todo) }}" method="post" style="display: inline;">
-            @csrf
-            <input type="submit" value="削除">
-          </form>
+        <li class="list-group-item" style="list-style: none;">
+          <div class="row">
+            <div class="col-sm-10">
+              <form action="{{ action('TodosController@status', $todo) }}" method="post" id="form_{{ $todo->id }}">
+                @csrf
+              </form>
+              @if ($todo->status == 0)
+                <a href="#" class="status" data-id="{{ $todo->id }}">
+                  <i class="far fa-square"></i>
+                </a>
+              @else
+                <a href="#" class="status" data-id="{{ $todo->id }}">
+                  <i class="far fa-check-square"></i>
+                </a>
+              @endif
+              <span style="padding-left: 5px;">{{ $todo->body }}</span>
+            </div>
+            <div class="col-sm-2">
+              <a href="{{ action('TodosController@edit', $todo) }}">編集</a>
+              <a href="#" data-id="{{ $todo->id }}" class="del" style="color: tomato;">削除</a>
+              <form action="{{ action('TodosController@destroy', $todo) }}" method="post" id="del_{{ $todo->id }}">
+                @csrf
+              </form>
+            </div>
+          </div>
         </li>
       @endforeach
     </ul>
   </div>
 </div>
 <script src="/js/status.js"></script>
+<script src="/js/del.js"></script>
 @endsection
